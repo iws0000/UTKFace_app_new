021445537b3e435d45b77a473a2e6217eb017328
iws0000 <jtx.rektwz@gmail.com>
2023-01-22 14:04:33 +0900

    変更内容

--- UTKFace.py
+++ UTKFace.py
@@ -9,9 +9,9 @@ import subprocess
 
 # モデルをダウンロードするコマンドを実行する
 command = '''
-curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1ICiciVdYM9VIfJXKRhBbpZGD6UIooDq6" > /dev/null
+curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1o0ltPnpnjT7Ml-GK_UAQ37qXvi_PL8C9" > /dev/null
 CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)" 
-curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1ICiciVdYM9VIfJXKRhBbpZGD6UIooDq6" -o my_model.h5
+curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1o0ltPnpnjT7Ml-GK_UAQ37qXvi_PL8C9" -o my_model.h5
 '''    
 subprocess.run(command, shell=True)
 
